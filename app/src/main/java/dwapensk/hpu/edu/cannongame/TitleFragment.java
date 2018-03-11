06ac469735d6d650883fe058340881e93006a80a
DonaldWapenski <dwapensk@my.hpu.edu>
2018-03-10 15:37:59 -1000

    Update and rename TitleFragment to TitleFragment.java

--- app/src/main/java/dwapensk/hpu/edu/cannongame/TitleFragment.java
+++ app/src/main/java/dwapensk/hpu/edu/cannongame/TitleFragment.java
@@ -3,17 +3,15 @@ package dwapensk.hpu.edu.cannongame;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-
 /**
  * Created by kiev on 3/10/2018.
  */
-
 public class TitleFragment extends Fragment {
-
     private Button mStartButton;
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savdInstanceState) {
@@ -22,16 +20,13 @@ public class TitleFragment extends Fragment {
         mStartButton = (Button) view.findViewById(R.id.start_game);
         mStartButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                Intent intent = new Intent(getActivity(), MainActivityFragment.class);
-                startActivity(intent);
-
+                getActivity().getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.fragment_container, new MainActivityFragment(),"findThisFragment")
+                        .addToBackStack(null)
+                        .commit();
             }
-
-
-
         });
 
         return view;
     }
-
 }
